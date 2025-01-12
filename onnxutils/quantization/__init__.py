24b296ad4a869b3d460efc2cb7ac39bfcd7bff71
ycgao <yc_x@outlook.com>
2025-01-12 22:09:11 +0800

    update

--- onnxutils/quantization/__init__.py
+++ onnxutils/quantization/__init__.py
@@ -2,3 +2,5 @@ from .common import symbolic_trace
 from .metric import compute_metric, cosine_kernel, snr_kernel, mse_kernel
 
 from .quantizer import QuantizerBase
+from .node_quantizer import NodeQuantizer
+from .module_quantizer import ModuleQuantizer
