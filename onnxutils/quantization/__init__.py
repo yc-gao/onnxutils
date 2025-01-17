18cffbd4adfcf3b5af590d46b12ae38bd07586dc
yinchaogao <yinchaogao@deeproute.ai>
2025-01-17 17:32:09 +0800

    update

--- onnxutils/quantization/__init__.py
+++ onnxutils/quantization/__init__.py
@@ -1,5 +1,3 @@
-from .metric import compute_metric, cosine_kernel, snr_kernel, mse_kernel
-
 from .quantizer import symbolic_trace, QuantizerBase
 from .node_quantizer import NodeQuantizer
 from .module_quantizer import ModuleQuantizer
