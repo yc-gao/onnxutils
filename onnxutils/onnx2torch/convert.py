bc62945d892921940ef3469e80d7903771443df1
ycgao <yc_x@outlook.com>
2025-02-17 14:07:47 +0800

    update onnx2torch

--- onnxutils/onnx2torch/convert.py
+++ onnxutils/onnx2torch/convert.py
@@ -8,14 +8,6 @@ from onnxutils.onnx import OnnxModel
 from .node_converter.registry import find_converter
 
 
-class InitializersContainer(torch.nn.Module):
-    def add_initializer(self, name: str, initializer: torch.Tensor) -> None:
-        self.register_buffer(name, initializer)
-
-    def forward(self, *args, **kwargs):
-        raise NotImplementedError
-
-
 def normalize_module_name(name, domain='', op_type=''):
     name = name or op_type
     name = name.replace('.', '_')
@@ -30,14 +22,10 @@ def convert(
     opset_import = {
         opsetid_proto.domain: opsetid_proto.version for opsetid_proto in onnx_model.opsets()}
 
-    root_initializer = InitializersContainer()
-
     root_module = torch.nn.Module()
-    root_module.add_module('initializers', root_initializer)
+    root_graph = torch.fx.Graph()
 
-    torch_graph = torch.fx.Graph()
-
-    torch_nodes = {}
+    nodes_mapping = {}
 
     # create input nodes
     for idx, name in enumerate(onnx_model.input_names()):
@@ -48,7 +36,7 @@ def convert(
             placeholder_name = name
         else:
             placeholder_name = f'input_{idx}'
-        torch_nodes[name] = torch_graph.placeholder(name=placeholder_name)
+        nodes_mapping[name] = root_graph.placeholder(name=placeholder_name)
 
     for onnx_node in onnx_model.nodes():
         converter = find_converter(
@@ -59,6 +47,7 @@ def convert(
 
         torch_module, onnx_mapping = converter(onnx_node, onnx_model)
         torch_module.onnx_mapping = onnx_mapping
+
         root_module.add_module(
             normalize_module_name(onnx_node.name()),
             torch_module
@@ -66,65 +55,61 @@ def convert(
 
         args = []
         for value_name in onnx_mapping.get('inputs', []):
-            if onnx_model.get_input_by_name(value_name) is not None:
-                args.append(torch_nodes[value_name])
-            elif onnx_model.get_initializer_by_name(value_name) is not None:
-                if value_name not in torch_nodes:
-                    initializer_value = onnx_model.get_initializer_by_name(
-                        value_name).to_torch()
+            if onnx_model.get_input_by_name(value_name):
+                args.append(nodes_mapping[value_name])
+            elif initializer_value := onnx_model.get_initializer_by_name(value_name):
+                if value_name not in nodes_mapping:
+                    initializer_value = initializer_value.to_torch()
                     initializer_value.onnx_mapping = {
                         'name': value_name,
-                        'is_parameter': value_name in onnx_mapping.get('params', [])
                     }
-                    buffer_idx = sum(
-                        1 for _ in root_initializer.buffers())
-                    buffer_name = f'onnx_initializer_{buffer_idx}'
-                    root_initializer.add_initializer(
-                        buffer_name,
-                        initializer_value,
-                    )
-                    torch_nodes[value_name] = torch_graph.get_attr(
-                        f'initializers.{buffer_name}')
-                args.append(torch_nodes[value_name])
+                    buffer_count = sum(1 for _ in root_module.buffers())
+                    buffer_name = f'onnx_initializer_{buffer_count}'
+                    root_module.register_buffer(buffer_name, initializer_value)
+                    nodes_mapping[value_name] = root_graph.get_attr(
+                        buffer_name)
+                args.append(nodes_mapping[value_name])
             elif onnx_input_node := onnx_model.get_node_by_output(value_name):
-                torch_input_node = torch_nodes[onnx_input_node.name()]
+                torch_input_node = nodes_mapping[onnx_input_node.name()]
                 if len(onnx_input_node.outputs()) > 1:
                     index = onnx_input_node.outputs().index(value_name)
-                    maybe_torch_input_node = torch_nodes.get(
+                    maybe_torch_input_node = nodes_mapping.get(
                         (torch_input_node, index),
                         None)
                     if maybe_torch_input_node is None:
-                        maybe_torch_input_node = torch_graph.call_function(
+                        maybe_torch_input_node = root_graph.call_function(
                             getitem, args=(torch_input_node, index))
-                        torch_nodes[(torch_input_node, index)] = maybe_torch_input_node
+                        nodes_mapping[(torch_input_node, index)
+                                      ] = maybe_torch_input_node
                     torch_input_node = maybe_torch_input_node
                 args.append(torch_input_node)
             else:
                 warnings.warn(
                     f'Got unexpected input value name ({value_name})')
 
-        torch_nodes[onnx_node.name()] = torch_graph.call_module(
-            module_name=normalize_module_name(onnx_node.name()), args=tuple(args))
+        nodes_mapping[onnx_node.name()] = root_graph.call_module(
+            module_name=normalize_module_name(onnx_node.name()),
+            args=tuple(args))
 
     args = []
     for output_name in onnx_model.output_names():
         onnx_output_node = onnx_model.get_node_by_output(output_name)
         assert onnx_output_node is not None
-        torch_output_node = torch_nodes[onnx_output_node.name()]
+        torch_output_node = nodes_mapping[onnx_output_node.name()]
         if len(onnx_output_node.outputs()) > 1:
             index = onnx_output_node.outputs().index(output_name)
-            torch_output_node = torch_graph.call_function(
+            torch_output_node = root_graph.call_function(
                 getitem, args=(torch_output_node, index))
         args.append(torch_output_node)
 
     if len(args) > 1:
-        torch_graph.output(tuple(args))
+        root_graph.output(tuple(args))
     else:
-        torch_graph.output(args[0])
+        root_graph.output(args[0])
 
-    torch_graph.lint()
+    root_graph.lint()
 
-    torch_model = torch.fx.GraphModule(root=root_module, graph=torch_graph)
+    torch_model = torch.fx.GraphModule(root=root_module, graph=root_graph)
     setattr(torch_model,
             'onnx_mapping',
             {
