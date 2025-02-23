32a42d73863f55dcfdb1f6bbbd6cc9b65807a778
ycgao <yc_x@outlook.com>
2025-02-23 23:20:48 +0800

    update

--- onnxutils/onnx2torch/node_converter/global_avg_pool.py
+++ onnxutils/onnx2torch/node_converter/global_avg_pool.py
@@ -6,9 +6,9 @@ from onnxutils.onnx import OnnxModel, OnnxNode
 from ..converter_registry import add_converter
 
 op_mapping = {
-    1: nn.AvgPool1d,
-    2: nn.AvgPool2d,
-    3: nn.AvgPool3d,
+    1: nn.AdaptiveAvgPool1d,
+    2: nn.AdaptiveAvgPool2d,
+    3: nn.AdaptiveAvgPool3d,
 }
 
 
@@ -16,9 +16,10 @@ op_mapping = {
 def _(onnx_node: OnnxNode, onnx_model: OnnxModel):
     shape = tuple(x.dim_value
                   for x in onnx_model.get_vinfo_by_name(
-                      onnx_node.inputs()[0]).type.tensor_type.shape.dim)
+                      onnx_node.outputs()[0]).type.tensor_type.shape.dim)
 
-    torch_module = op_mapping[len(shape) - 2](kernel_size=shape[2:])
+    # TODO(yinchao): must have channel axis
+    torch_module = op_mapping[len(shape) - 2](output_size=shape[2:])
     onnx_mapping = {
         'inputs': onnx_node.inputs(),
         'outputs': onnx_node.outputs(),
--- onnxutils/tools/onnx2torch.py
+++ onnxutils/tools/onnx2torch.py
@@ -1,12 +1,30 @@
 #!/usr/bin/env python3
 import argparse
 
+import numpy as np
+import onnx
+import torch
+
+from onnxutils.common.random import rand_numpy
 from onnxutils.onnx import OnnxModel
 from onnxutils.onnx2torch import convert
 
 
+def tensor_from_vinfo(vinfo):
+    dtype_mapping = {
+        onnx.TensorProto.DataType.FLOAT16: np.float16,
+        onnx.TensorProto.DataType.FLOAT: np.float32,
+        onnx.TensorProto.DataType.DOUBLE: np.float64,
+    }
+    shape = tuple(x.dim_value
+                  for x in vinfo.type.tensor_type.shape.dim)
+    dtype = vinfo.type.tensor_type.elem_type
+    return torch.from_numpy(rand_numpy(shape, dtype_mapping[dtype]))
+
+
 def parse_options():
     parser = argparse.ArgumentParser()
+    parser.add_argument('-o', '--output')
     parser.add_argument('model')
     return parser.parse_args()
 
@@ -21,7 +39,16 @@ def main():
                 node.name = sess.unique_name()
 
     torch_model = convert(onnx_model, False)
-    print(torch_model)
+    torch_model.print_readable()
+
+    if options.output:
+        torch.onnx.export(
+            torch_model,
+            tuple(tensor_from_vinfo(x) for x in onnx_model.inputs()),
+            options.output,
+            input_names=torch_model.onnx_mapping['inputs'],
+            output_names=torch_model.onnx_mapping['outputs'],
+        )
 
 
 if __name__ == "__main__":
