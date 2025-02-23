927db103558d954f227314c151e186adadc9aace
ycgao <yc_x@outlook.com>
2025-02-23 20:45:07 +0800

    update

--- onnxutils/onnx/__init__.py
+++ onnxutils/onnx/__init__.py
@@ -1,6 +1,6 @@
+from .pass_registry import add_optimizer, find_optimizer, apply_optimizers, list_optimizers
+from . import onnx_pass
+
 from .onnx_tensor import OnnxTensor
 from .onnx_node import OnnxNode
 from .onnx_model import OnnxModel
-
-from .pass_registry import add_optimizer, find_optimizer, apply_optimizers, list_optimizers
-from . import onnx_pass
