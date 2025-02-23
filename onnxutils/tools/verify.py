4bcdf4db9ff29cf44fff65ebf7f8de566a09bf46
ycgao <yc_x@outlook.com>
2025-02-23 22:20:23 +0800

    update

--- onnxutils/tools/verify.py
+++ onnxutils/tools/verify.py
@@ -4,6 +4,8 @@ import argparse
 import numpy as np
 import onnxruntime as ort
 
+from onnxutils.common.random import rand_numpy
+
 
 class RandomDataset:
     str2dtype = {
@@ -24,8 +26,7 @@ class RandomDataset:
             return None
         self.idx += 1
         return {
-            f.name: np.random.rand(
-                *f.shape).astype(RandomDataset.str2dtype[f.type])
+            f.name: rand_numpy(f.shape, RandomDataset.str2dtype[f.type])
             for f in self.fields
         }
 
@@ -43,46 +44,10 @@ class RandomDataset:
         return self
 
 
-def do_verify(model0, model1, rtol=1e-4, atol=1e-4):
-    sess0 = ort.InferenceSession(
-        model0,
-        providers=[
-            x for x in ['CUDAExecutionProvider', 'CPUExecutionProvider']
-            if x in ort.get_available_providers()
-        ])
-    sess1 = ort.InferenceSession(
-        model1,
-        providers=[
-            x for x in ['CUDAExecutionProvider', 'CPUExecutionProvider']
-            if x in ort.get_available_providers()
-        ])
-    dataset = RandomDataset(sess0.get_inputs(), 10)
-    for data in dataset:
-        sess0_outputs = sess0.run(None, data)
-        sess1_outputs = sess1.run(None, data)
-        for node, output0, output1 in zip(sess0.get_outputs(), sess0_outputs, sess1_outputs):
-            is_ok = np.allclose(output0, output1, rtol, atol)
-            if not is_ok:
-                print(f"verify field[{node.name}] failed")
-                max_val = output0.max()
-                min_val = output0.min()
-                mean_val = output0.mean()
-                print(f"output0 {max_val} {min_val} {mean_val}")
-                max_val = output1.max()
-                min_val = output1.min()
-                mean_val = output1.mean()
-                print(f"output1 {max_val} {min_val} {mean_val}")
-                tmp = np.absolute(output0 - output1)
-                max_val = tmp.max()
-                min_val = tmp.min()
-                mean_val = tmp.mean()
-                print(f"diff {max_val} {min_val} {mean_val}")
-            else:
-                print(f"verify field[{node.name}]...passed")
-
-
 def parse_options():
     parser = argparse.ArgumentParser()
+    parser.add_argument('--rtol', type=float, default=1e-5)
+    parser.add_argument('--atol', type=float, default=1e-5)
     parser.add_argument('model0')
     parser.add_argument('model1')
     return parser.parse_args()
@@ -90,7 +55,42 @@ def parse_options():
 
 def main():
     options = parse_options()
-    do_verify(options.model0, options.model1)
+
+    sess0 = ort.InferenceSession(
+        options.model0,
+        providers=[
+            x for x in ['CUDAExecutionProvider', 'CPUExecutionProvider']
+            if x in ort.get_available_providers()
+        ])
+    sess1 = ort.InferenceSession(
+        options.model1,
+        providers=[
+            x for x in ['CUDAExecutionProvider', 'CPUExecutionProvider']
+            if x in ort.get_available_providers()
+        ])
+    dataset = RandomDataset(sess0.get_inputs(), 100)
+    for data in dataset:
+        sess0_outputs = sess0.run(None, data)
+        sess1_outputs = sess1.run(None, data)
+        for node, output0, output1 in zip(sess0.get_outputs(), sess0_outputs, sess1_outputs):  # noqa
+            is_ok = np.allclose(output0, output1, options.rtol, options.atol)
+            if is_ok:
+                print(f"verify field '{node.name}' passed")
+            else:
+                print(f"verify field '{node.name}' failed")
+                max_val = output0.max()
+                min_val = output0.min()
+                mean_val = output0.mean()
+                print(f"    {max_val} {min_val} {mean_val}")
+                max_val = output1.max()
+                min_val = output1.min()
+                mean_val = output1.mean()
+                print(f"    {max_val} {min_val} {mean_val}")
+                tmp = output0 - output1
+                max_val = tmp.max()
+                min_val = tmp.min()
+                mean_val = tmp.mean()
+                print(f"    {max_val} {min_val} {mean_val}")
 
 
 if __name__ == "__main__":
