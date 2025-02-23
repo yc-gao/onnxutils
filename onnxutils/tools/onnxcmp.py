35afa10a070404cfd39c01f9a85f0e26a7e31f7d
ycgao <yc_x@outlook.com>
2025-02-23 22:29:07 +0800

    update

--- onnxutils/tools/onnxcmp.py
+++ onnxutils/tools/onnxcmp.py
@@ -4,28 +4,33 @@ import argparse
 import onnxutils.onnx
 
 
-def dfscmp(onnx_model0, onnx_node0, onnx_model1, onnx_node1):
+def dfscmp(onnx_model0, onnx_model1, output_name0, output_name1):
+    onnx_node0 = onnx_model0.get_node_by_output(output_name0)
+    onnx_node1 = onnx_model1.get_node_by_output(output_name1)
+
     if onnx_node0 is None and onnx_node1 is None:
         return
 
     if onnx_node0 is None or onnx_node1 is None:
-        print('node not same',
-              onnx_node0.name() if onnx_node0 else onnx_node0,
-              onnx_node1.name() if onnx_node1 else onnx_node1,
-              )
+        print('node diff')
+        print(f"    {onnx_node0.name() if onnx_node0 else onnx_node0}")
+        print(f"    {onnx_node1.name() if onnx_node1 else onnx_node1}")
         return
 
     if onnx_node0.op_type() != onnx_node1.op_type():
-        print('node not same', onnx_node0.name(), onnx_node1.name())
+        print('node diff')
+        print(f"    {onnx_node0.name() if onnx_node0 else onnx_node0}")
+        print(f"    {onnx_node1.name() if onnx_node1 else onnx_node1}")
         return
 
     if len(onnx_node0.inputs()) != len(onnx_node1.inputs()):
-        print('node not same', onnx_node0.name(), onnx_node1.name())
+        print('node diff')
+        print(f"    {onnx_node0.name() if onnx_node0 else onnx_node0}")
+        print(f"    {onnx_node1.name() if onnx_node1 else onnx_node1}")
         return
 
-    for input_name0, input_name1 in zip(onnx_node0.inputs(), onnx_node1.inputs()):
-        dfscmp(onnx_model0, onnx_model0.get_node_by_output(input_name0),
-               onnx_model1, onnx_model1.get_node_by_output(input_name1))
+    for input_name0, input_name1 in zip(onnx_node0.inputs(), onnx_node1.inputs()):  # noqa
+        dfscmp(onnx_model0, onnx_model1, input_name0, input_name1)
 
 
 def parse_options():
@@ -40,14 +45,13 @@ def main():
     onnx_model0 = onnxutils.onnx.OnnxModel.from_file(options.model0)
     onnx_model1 = onnxutils.onnx.OnnxModel.from_file(options.model1)
 
-    if sorted(onnx_model0.output_names()) != sorted(onnx_model1.output_names()):
-        print('output not same', onnx_model0.output_names(),
-              onnx_model1.output_names())
+    if len(onnx_model0.output_names()) != len(onnx_model1.output_names()):  # noqa
+        print('output diff')
+        print(f'    {onnx_model0.output_names()}')
+        print(f'    {onnx_model1.output_names()}')
 
-    for output_name in zip(onnx_model0.output_names()):
-        onnx_node0 = onnx_model0.get_node_by_output(output_name)
-        onnx_node1 = onnx_model1.get_node_by_output(output_name)
-        dfscmp(onnx_model0, onnx_node0, onnx_model1, onnx_node1)
+    for output_name0, output_name1 in zip(onnx_model0.output_names(), onnx_model1.output_names()):  # noqa
+        dfscmp(onnx_model0, onnx_model1, output_name0, output_name1)
 
 
 if __name__ == "__main__":
