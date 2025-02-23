d3e3c66b5fc4967c841df3711ef9113194bac5c0
ycgao <yc_x@outlook.com>
2025-02-23 22:08:48 +0800

    update

--- onnxutils/tools/optim.py
+++ onnxutils/tools/optim.py
@@ -2,36 +2,41 @@
 import argparse
 
 from onnxutils.onnx import OnnxModel
-from onnxutils.onnx import apply_optimizers
+from onnxutils.onnx import apply_optimizers, list_optimizers
 
 
 def parse_options():
     parser = argparse.ArgumentParser()
     parser.add_argument('-o', '--output')
+    parser.add_argument('-l', '--ls-optim', action='store_true', default=False)
     parser.add_argument('--optim', action='append', default=[])
-    parser.add_argument('model')
+    parser.add_argument('model', nargs='?')
     return parser.parse_args()
 
 
 def main():
     options = parse_options()
 
-    onnx_model = OnnxModel.from_file(options.model)
-    with onnx_model.session() as sess:
-        for node in onnx_model.proto().graph.node:
-            if node.name == '':
-                node.name = sess.unique_name()
+    if options.ls_optim:
+        print('\n'.join(list_optimizers()))
 
-    onnx_model = apply_optimizers(onnx_model, options.optim)
-    onnx_model.topological_sort()
+    if options.model:
+        onnx_model = OnnxModel.from_file(options.model)
+        with onnx_model.session() as sess:
+            for node in onnx_model.proto().graph.node:
+                if node.name == '':
+                    node.name = sess.unique_name()
 
-    with onnx_model.session() as sess:
-        for node in onnx_model.proto().graph.node:
-            if node.name == '':
-                node.name = sess.unique_name()
+        onnx_model = apply_optimizers(onnx_model, options.optim)
+        onnx_model.topological_sort()
 
-    if options.output:
-        onnx_model.save(options.output)
+        with onnx_model.session() as sess:
+            for node in onnx_model.proto().graph.node:
+                if node.name == '':
+                    node.name = sess.unique_name()
+
+        if options.output:
+            onnx_model.save(options.output)
 
 
 if __name__ == "__main__":
