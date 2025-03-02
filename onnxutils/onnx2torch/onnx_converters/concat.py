a23cb7aadb12139def69ecde105fb28cd7307cb3
ycgao <yc_x@outlook.com>
2025-03-02 23:04:10 +0800

    add concat

--- onnxutils/onnx2torch/onnx_converters/concat.py
+++ onnxutils/onnx2torch/onnx_converters/concat.py
@@ -1,20 +1,24 @@
+import torch
 from torch import nn
 from onnxutils.onnx import OnnxNode, OnnxModel
 
 from ..converter_registry import add_converter
 
 
-class TorchIdentity(nn.Module):
-    def __init__(self) -> None:
+class TorchConcat(nn.Module):
+    def __init__(self, axis: int):
         super().__init__()
+        self.axis = axis
 
-    def forward(self, x):
-        return x
+    def forward(self, *args):
+        return torch.cat(args, self.axis)
 
 
-@add_converter(op_type='Identity', version=16)
+@add_converter(op_type='Concat', version=13)
 def _(onnx_node: OnnxNode, _: OnnxModel):
-    torch_module = TorchIdentity()
+    axis: int = int(onnx_node.attrs['axis'])  # type: ignore
+
+    torch_module = TorchConcat(axis)
     onnx_mapping = {
         'name': onnx_node.name,
         'inputs': onnx_node.input_names,
