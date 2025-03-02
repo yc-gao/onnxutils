a1214cf978e46b183b433d38fe183441a35596e5
ycgao <yc_x@outlook.com>
2025-03-02 22:57:49 +0800

    add clip

--- onnxutils/onnx2torch/onnx_converters/__init__.py
+++ onnxutils/onnx2torch/onnx_converters/__init__.py
@@ -1,3 +1,4 @@
 from . import identity
 from . import activations
 from . import unary_math_operations
+from . import clip
---
+++ onnxutils/onnx2torch/onnx_converters/clip.py
@@ -0,0 +1,34 @@
+import torch
+from torch import nn
+from onnxutils.onnx import OnnxNode, OnnxModel
+
+from ..converter_registry import add_converter
+
+
+class TorchClip(nn.Module):
+    def __init__(self, min_val=None, max_val=None):
+        super().__init__()
+        self.min_val = min_val
+        self.max_val = max_val
+
+    def forward(self, x):
+        return torch.clip(x, self.min_val, self.max_val)
+
+
+@add_converter(op_type='Clip', version=13)
+def _(onnx_node: OnnxNode, onnx_model: OnnxModel):
+    min_val = onnx_model.get_initializer_by_name(onnx_node.input_names[1])
+    max_val = onnx_model.get_initializer_by_name(onnx_node.input_names[2])
+
+    if min_val is not None:
+        min_val = min_val.to_numpy().item()
+    if max_val is not None:
+        max_val = max_val.to_numpy().item()
+
+    torch_module = TorchClip(min_val, max_val)
+    onnx_mapping = {
+        'name': onnx_node.name,
+        'inputs': onnx_node.input_names[:1],
+        'outputs': onnx_node.output_names,
+    }
+    return torch_module, onnx_mapping
