08f8b4603ab840035edab5704d853f7b89bdd4fe
ycgao <yc_x@outlook.com>
2025-03-06 15:07:19 +0800

    add activations

--- onnxutils/onnx2torch/onnx_converters/__init__.py
+++ onnxutils/onnx2torch/onnx_converters/__init__.py
@@ -2,3 +2,5 @@ from . import identity
 
 from . import unary_math_operations
 from . import binary_math_operations
+
+from . import activations
---
+++ onnxutils/onnx2torch/onnx_converters/activations.py
@@ -0,0 +1,39 @@
+from torch import nn
+from onnxutils.onnx import OnnxNode, OnnxModel
+
+from ..converter_registry import add_converter
+
+
+@add_converter(op_type='Relu', version=14)
+def _(onnx_node: OnnxNode, _: OnnxModel):
+    torch_module = nn.ReLU()
+    onnx_mapping = {
+        'name': onnx_node.name,
+        'inputs': onnx_node.input_names,
+        'outputs': onnx_node.output_names,
+    }
+    return torch_module, onnx_mapping
+
+
+@add_converter(op_type='Sigmoid', version=13)
+def _(onnx_node: OnnxNode, _: OnnxModel):
+    torch_module = nn.Sigmoid()
+    onnx_mapping = {
+        'name': onnx_node.name,
+        'inputs': onnx_node.input_names,
+        'outputs': onnx_node.output_names,
+    }
+    return torch_module, onnx_mapping
+
+
+@add_converter(op_type='Softmax', version=13)
+def _(onnx_node: OnnxNode, _: OnnxModel):
+    axis = int(onnx_node.attrs.get('axis', -1))
+
+    torch_module = nn.Softmax(axis)
+    onnx_mapping = {
+        'name': onnx_node.name,
+        'inputs': onnx_node.input_names,
+        'outputs': onnx_node.output_names,
+    }
+    return torch_module, onnx_mapping
