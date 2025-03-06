b4898a2c684c6128a41a036ba3afe21f3764c7e7
ycgao <yc_x@outlook.com>
2025-03-06 18:03:33 +0800

    update

--- onnxutils/onnx2torch/onnx_converters/__init__.py
+++ onnxutils/onnx2torch/onnx_converters/__init__.py
@@ -1,11 +1,10 @@
-from . import identity
+from . import conv
+from . import gemm
 
 from . import unary_math_operations
 from . import binary_math_operations
 
-from . import conv
-from . import gemm
-
+from . import identity
 from . import activations
 from . import max_pool
 from . import max_avg_pool
